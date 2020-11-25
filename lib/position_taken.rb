99af07d179354fa01a785a7dfad38c80e0a2deaf
JR Ladson <jrladson@gmail.com>
2020-11-25 03:55:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " board[index] "" || board[index] == NIL
     return FALSE
-  else board[index] == "X" || "O"
+  else
     return TRUE
   end
 end
